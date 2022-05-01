ce187dfdbeb4b8da0f0d8a12e8e8c10f66a798f4
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-05-01 20:54:35 -0300

    feat: add Add (partial)

---
+++ src/pages/Add.tsx
@@ -0,0 +1,83 @@
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Divider,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import api, {
+  Category,
+  Discipline,
+  TeacherDisciplines,
+  Test,
+  TestByDiscipline,
+} from "../services/api";
+
+function Add() {
+  const navigate = useNavigate();
+  const { token } = useAuth();
+  const [terms, setTerms] = useState<TestByDiscipline[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    async function loadPage() {
+      if (!token) return;
+
+      const { data: testsData } = await api.getTestsByDiscipline(token);
+      setTerms(testsData.tests);
+      const { data: categoriesData } = await api.getCategories(token);
+      setCategories(categoriesData.categories);
+    }
+    loadPage();
+  }, [token]);
+
+  return (
+    <>
+      <h2>Adicione uma prova</h2>
+      <Divider sx={{ marginBottom: "35px" }} />
+      <Box
+        sx={{
+          marginX: "auto",
+          width: "700px",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          <Button
+            variant="outlined"
+            onClick={() => navigate("/app/disciplinas")}
+          >
+            Disciplinas
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => navigate("/app/pessoas-instrutoras")}
+          >
+            Pessoa Instrutora
+          </Button>
+          <Button variant="contained" onClick={() => navigate("/app/adicionar")}>
+            Adicionar
+          </Button>
+        </Box>
+        
+      </Box>
+    </>
+  );
+}
+
+
+export default Add;
